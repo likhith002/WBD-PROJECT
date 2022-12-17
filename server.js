62dc507614381f82c7da855e308843fc973df0cc
likhith002 <likhithbharadwaj@gmail.com>
2022-12-17 14:42:00 +0530

    Made change for library

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cors());
 
 // parse the request
 
-app.use("/images", express.static("images"));
+// app.use("/images", express.static("images"));
 // app.use('/', express.static(path.join(__dirname, "../electronicsmart-app/build")));
 
 const __dirname1 = path.resolve();
@@ -47,59 +47,59 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(bodyparser.json());
-app.use(express.json());
-const access = rotatefilestream.createStream("logfile.log", {
-  interval: "10m",
-  path: path.join(__dirname, "logs"),
-});
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use(bodyparser.urlencoded({ extended: true }));
+// app.use(bodyparser.json());
+// app.use(express.json());
+// const access = rotatefilestream.createStream("logfile.log", {
+//   interval: "10m",
+//   path: path.join(__dirname, "logs"),
+// });
 
-app.use("/", require("./electronicsmart-backend/server/routes/order_route"));
-app.use("/", require("./electronicsmart-backend/server/routes/reviews_route"));
-app.use("/", require("./electronicsmart-backend/server/routes/user_routes"));
-app.use("/", require("./electronicsmart-backend/server/routes/product_routes"));
-app.use(
-  "/",
-  require("./electronicsmart-backend/server/routes/price_history_routes")
-);
-app.use("/", require("./electronicsmart-backend/server/routes/offer_routes"));
-app.use(morgan("combined", { stream: access }));
-// connecting to mongo Atlas  server
-connectDB();
+// app.use("/", require("./electronicsmart-backend/server/routes/order_route"));
+// app.use("/", require("./electronicsmart-backend/server/routes/reviews_route"));
+// app.use("/", require("./electronicsmart-backend/server/routes/user_routes"));
+// app.use("/", require("./electronicsmart-backend/server/routes/product_routes"));
+// app.use(
+//   "/",
+//   require("./electronicsmart-backend/server/routes/price_history_routes")
+// );
+// app.use("/", require("./electronicsmart-backend/server/routes/offer_routes"));
+// app.use(morgan("combined", { stream: access }));
+// // connecting to mongo Atlas  server
+// connectDB();
 
-const store = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./images");
-  },
-  filename: function (req, file, cb) {
-    cb(null, crypto.randomUUID() + file.originalname);
-  },
-});
+// const store = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./images");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, crypto.randomUUID() + file.originalname);
+//   },
+// });
 
-const upload = multer({ storage: store });
+// const upload = multer({ storage: store });
 
-app.post("/avatar", upload.single("avatar"), async (req, res) => {
-  console.log(req.body);
-  console.log("dasd");
-  console.log(req.query);
+// app.post("/avatar", upload.single("avatar"), async (req, res) => {
+//   console.log(req.body);
+//   console.log("dasd");
+//   console.log(req.query);
 
-  let params = SeperateNonAttribParams(UserModel, req.body);
-  if (req.file) {
-    let tt = await UserModel.findOneAndUpdate(params, {
-      $set: { avatar: `localhost:${port}/images/${req.file.filename}` },
-    });
-    res.status(200).json(tt);
-  }
-});
+//   let params = SeperateNonAttribParams(UserModel, req.body);
+//   if (req.file) {
+//     let tt = await UserModel.findOneAndUpdate(params, {
+//       $set: { avatar: `localhost:${port}/images/${req.file.filename}` },
+//     });
+//     res.status(200).json(tt);
+//   }
+// });
 
-app.get("product/*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../electronicsmart-app/build", "index.html")
-  );
-});
+// app.get("product/*", (req, res) => {
+//   res.sendFile(
+//     path.join(__dirname, "../electronicsmart-app/build", "index.html")
+//   );
+// });
 
-app.listen(port, () => {
-  console.log(`server is running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`server is running at http://localhost:${port}`);
+// });
